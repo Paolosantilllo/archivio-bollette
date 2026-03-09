d7105bed17da241f0a6eb3ea27f0f59b95313b7a
Paolo Santillo <paolosantillo@yahoo.it>
2026-03-09 08:53:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+="57028"}
+let folders = JSON.parse(localStorage.getItem("folders")) || [];
 
+const list = document.getElementById("folders");
+const addBtn = document.getElementById("addFolder");
+
+function render(){
+list.innerHTML="";
+
+folders.forEach((f,i)=>{
+let li=document.createElement("li");
+li.className="folder";
+li.innerText="📁 "+f;
+list.appendChild(li);
+});
+
+localStorage.setItem("folders",JSON.stringify(folders));
+}
+
+addBtn.onclick=function(){
+
+let name=prompt("Nome cartella");
+
+if(name){
+folders.push(name);
+render();
+}
+
+}
