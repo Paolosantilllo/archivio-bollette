acdf02bf32a045b3362a32bab0d38a407c1923ec
Paolo Santillo <paolosantillo@yahoo.it>
2026-03-24 12:18:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-et data = JSON.parse(localStorage.getItem("archivio")) || [];
+let data = JSON.parse(localStorage.getItem("archivio")) || [];
 let currentPath = [];
 let currentPdfUrl = null;
 let currentViewerFile = null;
